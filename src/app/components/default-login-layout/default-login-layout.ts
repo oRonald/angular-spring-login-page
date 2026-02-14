1dd38d034be4401ae47c4f08263215dc4f4fc7d1
oRonold <or040918@gmail.com>
2026-02-14 14:47:52 -0300

    Create initial structure

---
+++ src/app/components/default-login-layout/default-login-layout.ts
@@ -0,0 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  imports: [NgOptimizedImage],
+  templateUrl: './default-login-layout.html',
+  styleUrl: './default-login-layout.scss',
+})
+export class DefaultLoginLayout {
+
+}
